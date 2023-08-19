243083d1b7bffa55390a18fcda1fbb51467ad4d1
anon <>
2023-08-19 13:09:52 -0700

    Rocks added

---
+++ src/lib/stores/ore.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const oreStore = writable(0);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { oreStore } from '$lib/stores/ore.js';
+
+    let ore=0;
+	oreStore.subscribe((value) => {
+		ore = value;
+	});
+
+    
+    async function mine() {
+        oreStore.update((currentOre) => {
+            return currentOre + 1
+        })
+    }
+</script>
+
+<h1>Welcome to the Mines</h1>
+<p>You have {ore} ore</p>
+<p>Mine rocks to get more ore</p>
+
+<button on:click={mine}>mine!</button>
