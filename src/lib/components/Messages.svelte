e3ec8e6b6a41d3c1617e97abeeea79d645670530
anon <>
2023-08-22 22:42:13 -0700

    Actually fixed messages updating

--- src/lib/components/Messages.svelte
+++ src/lib/components/Messages.svelte
@@ -4,35 +4,27 @@
     import { player } from '$lib/stores/player'
 
     let filterType = 'ALL'
-    $: filteredMessages = changeFilter(filterType)
+    let filteredMessages = $messages
 
-    /**
-     * @param {string} type
-     */
-    function changeFilter(type) {
-        if (type != 'ALL') {
-            filterType = type
-            return $messages.filter(obj => {
-                return obj.type === type;
-            });
-        } else {
-            filterType = 'ALL'
-            return $messages
-        }
+    $: if (filterType != 'ALL') {
+        filteredMessages = [...$messages].filter(obj => obj.type === filterType)
+    } else {
+        filteredMessages = $messages
     }
+
 </script>
 
 <div class="container w-full">
     <p class="font-bold">Filter messages by type: {filterType}</p>
     <div class="flex">
         {#if $player.dogs.amount >0}
-            <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('DOGS')}>dogs</button>
+            <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'DOGS'}>dogs</button>
         {/if}
-        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('APES')}>apes</button>
-        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('ORE')}>ore</button>
-        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('STATE')}>state</button>
-        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('GREETING')}>greeting</button>
-        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => changeFilter('ALL')}>all</button>
+        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'APES'}>apes</button>
+        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'ORE'}>ore</button>
+        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'STATE'}>state</button>
+        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'GREETING'}>greeting</button>
+        <button class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={() => filterType = 'ALL'}>all</button>
     </div>
     <ul>
         {#each filteredMessages as message}
