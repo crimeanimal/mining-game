f17bad78c04a9de6d73b38d8406e01f41dd8e482
anon <>
2023-08-26 16:49:33 -0700

    Added Sortable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,14 @@
     import Save from '$lib/components/Save.svelte';
     import Store from '$lib/components/Store.svelte';
     import Market from '$lib/components/Market.svelte';
+
+    import Sortable from 'sortablejs';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        let el = document.getElementById("items")
+        let sortable = Sortable.create(el);
+    })
 </script>
 
 <svelte:head>
@@ -12,12 +20,12 @@
 </svelte:head>
 
 <div style="height: 100vh;" class="dark:text-gray-100 w-full h-full mx-0 pt-16">
-    <div style="height: 100vh;" class="container mx-auto grid md:grid-cols-2 xl:grid-cols-3">
-        <Mine />
-        <Market />
-        <ApeTerminal />
-        <Store />
-        <Messages />
-        <Save />
-    </div>
+    <ul id="items" style="height: 100vh;" class="container mx-auto grid md:grid-cols-2 xl:grid-cols-3">
+        <li><Mine /></li>
+        <li><Market /></li>
+        <li><ApeTerminal /></li>
+        <li><Store /></li>
+        <li><Messages /></li>
+        <li><Save /></li>
+    </ul>
 </div>
