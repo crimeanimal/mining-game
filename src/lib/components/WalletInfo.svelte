5a9b23a4fd701f6e04dc46bb5f0974424bae430f
anon <>
2023-08-26 17:03:24 -0700

    Made WalletInfo component

---
+++ src/lib/components/WalletInfo.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { wallet } from "$lib/stores/wallet";
+    import { player } from "$lib/stores/player";
+    import bayc1 from '$lib/images/bayc1.webp'
+    import bayc2 from '$lib/images/bayc2.webp'
+    import bayc3 from '$lib/images/bayc3.webp'
+    import bayc4 from '$lib/images/bayc4.webp'
+    import bayc5 from '$lib/images/bayc5.webp'
+    import broke from '$lib/images/broke.gif'
+    let bayc = [bayc1,bayc2,bayc3,bayc4,bayc5]
+    let profilePic = bayc[Math.floor(Math.random() * bayc.length)]
+
+    
+    function connectWallet() {
+        $wallet.connected = true
+    }
+</script>
+
+<div>
+    {#if $wallet.connected}
+        <div class="flex gap-3 pt-2">
+            <img class='h-10 w-10 rounded-full ring-2 ring-gray-100' src={$player.apes > 0 ? profilePic : broke} alt="">
+            <div class="self-center">
+                <p>0xf4rt_buck3t connected</p>
+                <p class="text-xs">{$player.apes} ape{#if $player.apes != 1 }s{/if}</p>
+            </div>
+        </div>
+    {:else}
+        <button class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={connectWallet}>Connect Wallet</button>
+    {/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,21 +9,10 @@
     import { ape } from "$lib/stores/ape";
     import { wallet } from "$lib/stores/wallet";
     import { player } from "$lib/stores/player";
-    import bayc1 from '$lib/images/bayc1.webp'
-    import bayc2 from '$lib/images/bayc2.webp'
-    import bayc3 from '$lib/images/bayc3.webp'
-    import bayc4 from '$lib/images/bayc4.webp'
-    import bayc5 from '$lib/images/bayc5.webp'
-    import broke from '$lib/images/broke.gif'
     import { formatter } from "$lib/formatter";
+    import WalletInfo from "$lib/components/WalletInfo.svelte";
 
-    let bayc = [bayc1,bayc2,bayc3,bayc4,bayc5]
-    let profilePic = bayc[Math.floor(Math.random() * bayc.length)]
     let songs = [fast_car,human,virtual_insanity]
-
-    function connectWallet() {
-        $wallet.connected = true
-    }
 
     function toggleSound() {
         $settings.sounds = !$settings.sounds
@@ -112,19 +101,7 @@
     </div>
     <div class="">
         <nav class="flex justify-end space-x-2 px-4">
-            <div>
-                {#if $wallet.connected}
-                    <div class="flex gap-3 pt-2">
-                        <img class='h-10 w-10 rounded-full ring-2 ring-gray-100' src={$player.apes > 0 ? profilePic : broke} alt="">
-                        <div class="self-center">
-                            <p>0xf4rt_buck3t connected</p>
-                            <p class="text-xs">{$player.apes} ape{#if $player.apes != 1 }s{/if}</p>
-                        </div>
-                    </div>
-                {:else}
-                    <button class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800" on:click={connectWallet}>Connect Wallet</button>
-                {/if}
-            </div>
+            <WalletInfo />
             <button on:click={toggleSound} class="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
                 {#if $settings.sounds}
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
